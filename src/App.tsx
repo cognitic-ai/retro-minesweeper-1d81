f24919d89ed6bf3d31843d54c6002d57570858b9
David Mokos <davidmokos@gmail.com>
2025-02-24 12:12:02 +0100

    Update references to sonner and ThemeProvider in code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { ThemeProvider } from "@/components/theme-provider"
-import { Toaster } from "@/components/ui/sonner"
-
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="container mx-auto min-h-screen flex flex-col items-center justify-center">
-        <h1 className="text-3xl font-bold">Hello World</h1>
-      </div>
-      <Toaster />
-    </ThemeProvider>
-  )
+    <div className="container mx-auto min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold">Hello World</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ThemeProvider } from "./components/theme-provider.tsx";
+import { Toaster } from "sonner";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <App />
-)
+    <Toaster />
+  </ThemeProvider>
+);
